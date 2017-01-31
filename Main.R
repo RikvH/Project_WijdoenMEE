bb0a618f0c1394571d4949020d9b473fa6da904a
Dillen Bruil <Dillen Bruil>
2017-01-31 09:36:58 +0100

    Updated main 31-1

--- Main.R
+++ Main.R
@@ -27,11 +27,14 @@ nedcrop <- crop(ned, c(5.65, 5.7,51.965, 51.99))
 ## Run scripts
 
   # Set area
-#setBaseOSM(5.672035, 51.975332, 1700, 1700)
+#wag <- setBaseOSM(5.672035, 51.975332, 1700, 1700)
   # Find shortest route
 
 
-route_points <- findRoute(51.9817026,5.6483571, 51.965421, 5.659061)
+route <- findRoute(51.9700752,5.6681647, 51.965421, 5.659061)
+# Make a spatial point data frame from the route 
+route_points <- as_sp(route, "points")
+#return (route_points)
 
 #route_points <- findRoute(51.9709465, 5.6689275, 51.978103, 5.6713907)
 
